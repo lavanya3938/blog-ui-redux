cda5ccb4e40d55686b572db8c15a4a7040b9e5a3
lavanya <lavanya3938@gmail.com>
2020-04-02 11:48:18 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Home from './Home'
+import UsersList from './UsersList'
+import PostsList from './PostsList'
+import UserShow from './UserShow'
+import PostShow from './PostShow'
+
+import {BrowserRouter, Route, Link} from 'react-router-dom'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+    <div>
+      <Link to="/">Home</Link> | &nbsp;
+      <Link to="/users">Users</Link> | &nbsp;
+      <Link to="/posts">Posts</Link>
+
+      <Route path="/" component={Home} exact={true} />
+      <Route path="/users" component={UsersList} exact ={true} />
+      <Route path="/posts" component={PostsList} exact={true}/>
+      <Route path="/users/:id" component={UserShow} />
+      <Route path="/posts/:id" component={PostShow} />
+
     </div>
+    </BrowserRouter>
   );
 }
 
